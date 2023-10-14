6f9c0701bbd62471c73dd9e5be97e669bbe2bc36
kyrosle <le@90e.com>
2023-10-14 21:25:57 +0800

    add logger to main crate

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,12 @@
-fn main() {}
+use flexi_logger::{Logger, opt_format};
+use nimbus_common::{log::info, ResultType};
+
+fn main() -> ResultType<()> {
+  let _logger = Logger::try_with_env_or_str("info")?
+    .log_to_stdout()
+    .format(opt_format)
+    .write_mode(flexi_logger::WriteMode::Async)
+    .start()?;
+  info!("logger init");
+  Ok(())
+}
