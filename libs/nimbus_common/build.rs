9cfbaed24bac07453cf2dffbfcfd097f59535007
kyrosle <le@90e.com>
2023-10-14 19:42:22 +0800

    add protobuf support

--- libs/nimbus_common/build.rs
+++ libs/nimbus_common/build.rs
@@ -1,3 +1,21 @@
-fn main() {
+use std::process::Command;
 
+fn main() {
+  let out_dir = "src/protos";
+
+  protobuf_codegen::Codegen::new()
+    .pure()
+    .out_dir(out_dir)
+    .inputs(["protos/message.proto"])
+    .include("protos")
+    .customize(protobuf_codegen::Customize::default().tokio_bytes(true))
+    .run()
+    .expect("Codegen failed.");
+  Command::new("cargo")
+    .args(["fmt"])
+    .current_dir("protos")
+    .output()
+    .unwrap();
+
+  println!("cargo:rerun-if-changed=protos/*.proto");
 }
