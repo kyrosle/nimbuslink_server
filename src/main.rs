85a239958d10e8b9583e63ad58b762c50ef55038
kyrosle <le@90e.com>
2023-10-15 17:22:55 +0800

    use colorful logout

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use flexi_logger::{Logger, opt_format};
 use nimbus_common::{log::info, ResultType};
+use flexi_logger::{colored_opt_format, Logger};
+use nimbus_common::{logger::*, ResultType};
 
 fn main() -> ResultType<()> {
   let _logger = Logger::try_with_env_or_str("info")?
     .log_to_stdout()
-    .format(opt_format)
+    .set_palette("196;208;80;7;8".to_string())
+    .format(colored_opt_format)
     .write_mode(flexi_logger::WriteMode::Async)
     .start()?;
-  info!("logger init");
+  info!("logger init finished");
   Ok(())
 }
